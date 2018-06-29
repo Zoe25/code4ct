ae55137a50414ea59945b3b267c7e9428d6a13e9
zoe <zoeestellethomas@gmail.com>
2018-06-29 17:00:15 +0200

    uploading code4ct project

---
+++ js/main.js
@@ -0,0 +1,54 @@
+//code for the countup
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 2000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      $this.addClass('done');
+    }
+
+  });
+
+
+
+});
+
+//code for the gallery
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("gallery");
+  var indicator = document.getElementsByClassName("slider-number");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < indicator.length; i++) {
+     indicator[i].className = indicator[i].className.replace(" indicated", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  indicator[slideIndex-1].className += " indicated";
+};
