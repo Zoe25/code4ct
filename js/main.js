6f622c3ad7b619014406fd6d4a209b58205f495f
zoe <zoeestellethomas@gmail.com>
2018-07-10 15:07:50 +0200

    updating

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+//
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+
 //code for the countup
 $('.counter').each(function() {
   var $this = $(this),
@@ -51,4 +80,4 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
   indicator[slideIndex-1].className += " indicated";
-};
+}
